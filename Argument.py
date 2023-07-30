8981b15048ef36e38ce2380ae3815a16539f4c0c
Prakashkakad <prakashkakad812@gmail.com>
2023-07-30 11:22:32 +0530

    Argument

---
+++ Argument.py
@@ -0,0 +1,10 @@
+#Default Paramenter value Argument.
+
+def school_name(name="S.C.S Jinour College"):
+    print("hello"+name)
+
+school_name( "Prakash")
+
+
+
+#Keyword Argument.
---
+++ Function.py
@@ -0,0 +1,15 @@
+'''def  calculator(a,b):
+    mean=(a+b)/(a-b)
+    print("Mean of these two number is :",mean)
+
+    if a <= b:
+        print("B is greater than a")
+    else :
+        print("a is greater then b" )
+
+
+a=int(input("Please enter the first number"))
+b=int(input("Please enter the second number"))
+
+calculator(a,b)
+# print("Mean is :",calculator(a,b))'''
