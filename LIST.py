57512010b94be14c2cdcca0d6efcc70ceee63f74
Prakashkakad <prakashkakad812@gmail.com>
2023-07-31 08:38:23 +0530

    Using

--- LIST.py
+++ LIST.py
@@ -1,4 +1,21 @@
-LIST_OF_MARKS =[9,10,7]
+#List
+'''LIST_OF_MARKS =[9,10,7]
 print(LIST_OF_MARKS)
 print(type(LIST_OF_MARKS))
-print(LIST_OF_MARKS(append))
+print(LIST_OF_MARKS(append))'''
+
+
+#Tuple
+family=tuple(("Man","women","Brother","sister"))
+# print(family)
+
+print(type(family),family)
+
+print(len(family))
+
+print(family[2
+      ])
+
+
+if ("sister") in family:
+    print("Family memeber contian sister")
--- Statement.py
+++ Statement.py
@@ -11,3 +11,12 @@
 #     if 'e' in word:
 #         continue
 #     print(f"{word} does not contain 'e'.")
+
+#-------------------------------Enumerate function
+'''marks=[79,43,83,99]
+for index,mark in enumerate(marks):
+    print(mark)
+    if (index==3):
+        print ("YOU have got nice marks")
+'''
+
